1cff92c0123bdf60d41bfad188a37582a11192dd
sangram <balsangram@gmail.com>
2025-04-08 12:05:26 +0530

    update live link

--- src/controllers/liveLink.controller.js
+++ src/controllers/liveLink.controller.js
@@ -1,16 +1,30 @@
 import LiveLink from "../models/LiveLink.js";
 
+// export const displayLiveLink = async (req, res) => {
+//   try {
+//     const live = await LiveLink.find();
+
+//     if (!live || live.length === 0) {
+//       return res.status(404).json({ message: "No live links found.",data:[] });
+//     }
+
+//     res
+//       .status(200)
+//       .json({ message: "Live links fetched successfully.", data: live });
+//   } catch (error) {
+//     console.error("Error fetching live links:", error);
+//     res.status(500).json({ message: "Internal server error." });
+//   }
+// };
+
 export const displayLiveLink = async (req, res) => {
   try {
     const live = await LiveLink.find();
 
-    if (!live || live.length === 0) {
-      return res.status(404).json({ message: "No live links found.",data:[] });
-    }
-
-    res
-      .status(200)
-      .json({ message: "Live links fetched successfully.", data: live });
+    res.status(200).json({
+      message: "Live links fetched successfully.",
+      data: live || [], // just in case
+    });
   } catch (error) {
     console.error("Error fetching live links:", error);
     res.status(500).json({ message: "Internal server error." });
