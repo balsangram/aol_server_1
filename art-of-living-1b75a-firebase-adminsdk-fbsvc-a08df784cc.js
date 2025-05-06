cac64b2b4fcf5ce20c09b831f91bd9964e7666ad
sangram <balsangram@gmail.com>
2025-05-06 14:15:33 +0530

    update some changes app deployed

--- art-of-living-1b75a-firebase-adminsdk-fbsvc-a08df784cc.js
+++ art-of-living-1b75a-firebase-adminsdk-fbsvc-a08df784cc.js
@@ -1,16 +1,15 @@
 export const serviceAccount = {
   type: "service_account",
-  project_id: "art-of-living-1b75a",
-  private_key_id: "a08df784cc6098ae04b60d18819919802edd800d",
+  project_id: "aolic-a3c42",
+  private_key_id: "f517b1fe88c9002367fa684ddbe25028310b107a",
   private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2uQ2CfKVSvVZw\n6aIStCsnJc3sb1bFCte/O+0yreHLREi8DHzsg+3GD0/GTZsKDj1lxsygooGwku3F\nWMzzOu4rOt6LHJeYx9RBRGolU+iLDAt++yIs3hzWJW3WQ/Jl2Jra3tIOnLiUK1hx\nAd1oebYlhuhp6qVY712rs+7PIVAEvgGRnto7tPsGqt8Qal2BgTkdFq05SNEFAFYt\nRYrZC6PTgweAXx4lu12PQDzElX5lgjwZQarHLW+fc6u1v1z8Ac/7suvv7A5PnDVP\n6PckQhfy3UhdRgPH3Vi34csBH3kLP56gBD5gf+zaZM0/GG4jTj8c5r3Td5ZdsB1Y\nD7TJB9VZAgMBAAECggEABeBZAouhrCcyAtBGCcCeQVRiQAAAqWDvjqiNuKXbmh7o\nZpZ8OgWwKEDRJE6AoEfUSlP6EWcd2/Y4YV1kEfp2MxEMm9Vf5rueeY7gCbKeIEKo\nkqfe8dR0LEroh7iIW3rQ2B3MYZ3sAw/geJntFfFmyI7Ye0ze48em7+EJ2sLL1lXR\nZqPtBBX91wRt3Fr7i5b1N/TpJpFgP2zK28A5b7XxtVbUQsaKysOokqhiAylFQJMF\nWAgbOCTyAqOH4Uu+dLaoBtpAL86Ptd5HHQmtszcjZzDdeGQX9CHsm6MkIw1JZtam\nnt13WcivkogA1GKAGNp+Fhrk6cdDBFt5OsJs++LVgQKBgQDkPVVPS1Mn10SOje9Q\n+EIAMsAU8KdSX0JI9Mu2C1HeVud/nJD57pKCOS6eKJiU9lXxFt7VMM0Y/2wrMxah\nm8AEBxvgh/ciivlnICoP3GCPanD5ULoWkx98ZDmGaBo0liVWNV63qLrMSIG460Xz\n+71WNYFK4asi7bUzxAFlz9UHaQKBgQDM8nihaGzta3Mp6UGHYq6/QxH/JNEW5zSF\n4x54N6y5Yx3JM/5fp2ApZb4ifaD0tWKOQIUD9IeC2NkUy5TiBq+Wu+ozR2UrXJXk\naoSNiKO3IlKPRVzMV/CVAFMmp4EFmzHUw2JAPWnd0hxTKCMRp6LaVkCaf+15B8mm\nOFBAryEQcQKBgQCg5XvWQkuEsoRp2jTy54/fg+dU27GYp/6aX7ebxa8ZBjp8A2tQ\nRZaSpqzuYb/ugxlis7SypYxwKt+eNI2nr5xVOo20YyUc/MuEn3AHblvDvk2V9Aq7\nD0wK7LyAu7J9j7sSYt06q3/Dsvl/2pudu8FlnKJgn2V9YgVzUKGLnE+YwQKBgFzU\ns2lA+J3t3qMsQX9GZqrBgYRVM9Qw86x1fJateTIbwCEjyOOg18DIkf/jcaWGB1cx\nyE5LU5okCOgPcqeSqbQX9/Q0MZIbhNWKnfHR434PMOAuZeM1Lgb/t6o5kwpsCn05\n5uX8K1jLVAMimgfkK7LEAM06huEccu00cr1p1gsxAoGAZ/MNFu59gCEYt5xnTP7+\nf49FFP6CAg1ESgVuqOyTGzHHm2PYpmP1R/Bt8oSM0hbLrpX9N5T/YYDWIduu6tc5\nvsJV9WAmguRIXIFFTygDp8tfSpAInv9I76+FcClW7hO1KDML45gOZLiOwg0EZvNp\nf+2UeoEb9I4TJUskpJl19HI=\n-----END PRIVATE KEY-----\n",
-  client_email:
-    "firebase-adminsdk-fbsvc@art-of-living-1b75a.iam.gserviceaccount.com",
-  client_id: "109132272880526430587",
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNvp9sbJIZLfeu\n6ahj0MQvXecdclGFkAzbS+e1aMcRpGXTwuElVUL0Qm0aKexO5HdYij4pjL2AiIxM\na0hr3ng9mfj+Hf6PewMp7csFq8peRjtHWj0R4aYCaYuK1y2HP0v4OHmTxVgjl4eq\nyM7Ckzl+3bdxG8vFbYUsQwaCYc75bZ0Wv2I05Qm0CE/n8rGSAMkJ1JJnGq6S9a6v\nlz1xmmFOR8Yt7knQFddzumoP8/IAsWBs/9WyKE6FiknRGuxZkwb/G0mmmUwyYKgP\nUhsaz3/VB0H0y3js+9JnBQgyFu6sXPOFKDrDGDcSgPyQJabiOIL0Uxju+Gel070l\n8A/IkJWlAgMBAAECggEAL3rSJ59Wh+EHy6G4bnB7FcNB1pXPIld/4AvASSkI55Vc\numW6t3bZoKzDqMoL8pjewdwFRGlqQgNA/VG0wM/yR6UExP//AvWMYVyzAXgHl4H1\npB27AilkuiHJRh0qMI/25C2EQQnFsVECyfLVZCm9lv1rm/qiJ8mCdhhxpB0WOpEb\nESyrVn7b4JHaQQUv1raj2q25ChF2s1XfDVGkoHvZezOQVfXeeESso9oHOv+uDv/q\nKgUfHoEus22zYTsTYaAN63BirXvUR9AH8ElprxnFYJDAxB0J04TbiVKj8TlcrdSX\nKw2zfeGVltpKTF6fXwqLvCs1V05XgX47EmRgQQGzcQKBgQD0WG7a1masHUtdI9FJ\nHLZob1glHCSy7mnvTB4BQQG7xDbu/AWmbTpykN8e/zAUavALONnHgWe2lGaJPHhO\nBoR36UuFFCD2YSXMTgyGG36RLBDrGxu/nrotFO+XrZeT8viOKKxscPt5XY+6kbzW\nZ5DK7CI8z+eWOg1iP+3MihnBZwKBgQDXjttAbSYabuWo/BDffRpDD5emX5GyKsyS\nCeKwisYcyY6l4OTW3/5zbyr/9goPTeJ9Ujj+fR7jj26FWuhHTyOw8n9L0ap56UK3\ndWrJmNEpQTL7bmh2mtrzSDq5ggm3FxO9dDvqSnFzj29PBCYgnbHlSDEa6S0jwWUM\n9QyTuWMNEwKBgFp54Fph51S16DEN0TH8UsKq7/2VxTUIOOulyLp4IrcvOZQWd9Uf\nPvQrFqM4pMn6qvXM0D8ieI/PtYP9gyKJIUXxechGIsp4QHG9OL/zsFL6+SzFNeon\nURLeXLVSTv02i7i/8NCaqVNQRKc1zbGqWZ/5/dd8uGkVB92ohFonjM/jAoGALH3h\nBVhCEByprIpylR6GSCc0vvNk6ChrVw1RS+0o3RJBLj3QHgXXyE2+y1wmUWhbAlDx\nUJ3UvVWCk2XjIazVChXTbnvVOxqJWcKTiw7OxTwe4MS2LRzjammTLW0IwlCyE12l\nbEN3TUniyKVrlTnXWSLgd9R/K60kRVtjAC64oh8CgYEA8RJuts1HrVo/qSqzoK0Q\nFuA5N5Hg3SL/mygS8kBVMtnecUtRpY6HFwY0Wg/tQRwzixSBVINLW9CDydcxUy/X\nOv+y7e56VPLJsk76VkzChTxPtNLiAxTqItNBbt+Si3w59NvfkkJMNalinN8S1gO5\ncaYcdNboL2jM2iv1U6eQwiM=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-fbsvc@aolic-a3c42.iam.gserviceaccount.com",
+  client_id: "101596503546946949609",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40art-of-living-1b75a.iam.gserviceaccount.com",
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40aolic-a3c42.iam.gserviceaccount.com",
   universe_domain: "googleapis.com",
 };
